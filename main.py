8305f527ddc72a6d54ae3b55d03deb81e0599a17
Sam Robertson <freechurro1@gmail.com>
2020-07-21 16:45:21 -0600

    restructured main.py

--- main.py
+++ main.py
@@ -5,40 +5,42 @@ from visualize_dataset import visualize_segmentation_dataset
 '''
 
 '''
+# whether or not this is being run on the compute canada machine
+cc = False
 
-# train paths
-train_frames_path = 'TissueDataset/Training/Original'
-train_masks_path = 'TissueDataset/Training/Mask'
-# train paths on compute canada machine
-cc_train_frames_path = '../TissueDataset1024/Training/Original'
-cc_train_masks_path =  '../TissueDataset1024/Training/Mask'
+def main():
+    # output paths
+    history_dir = './history_dev/'
+    output_dir = './predictions_dev/'
+    if cc:
+        # train paths
+        train_frames_path = '../TissueDataset1024/Training/Original'
+        train_masks_path = '../TissueDataset1024/Training/Mask'
+        # validation paths
+        val_frames_path = '../TissueDataset1024/Validation/Original'
+        val_masks_path = '../TissueDataset1024/Validation/Mask'
+        # test paths
+        test_frames_path = '../TissueDataset1024/Test/Original'
+        test_masks_path = '../TissueDataset1024/Test/Mask'
+    else:
+        # train paths
+        train_frames_path = 'TissueDataset/Training/Original'
+        train_masks_path = 'TissueDataset/Training/Mask'
+        # validation paths
+        val_frames_path = 'TissueDataset/Validation/Original'
+        val_masks_path = 'TissueDataset/Validation/Mask'
+        # test paths
+        test_frames_path = 'TissueDataset/Test/Original'
+        test_masks_path = 'TissueDataset/Test/Mask'
 
-# validation paths
-val_frames_path = 'TissueDataset/Validation/Original'
-val_masks_path = 'TissueDataset/Validation/Mask'
-# validation paths on compute canada machine
-cc_val_frames_path = '../TissueDataset1024/Validation/Original'
-cc_val_masks_path = '../TissueDataset1024/Validation/Mask'
+    # finetune the mini unet model
+    #finetuning_random(history_dir, train_frames_path, train_masks_path, val_frames_path, val_masks_path, test_frames_path, test_masks_path, num_models=100)
+    #finetuning_loop(history_dir, train_frames_path, train_masks_path, val_frames_path, val_masks_path, test_frames_path, test_masks_path)
 
-# test paths
-test_frames_path = 'TissueDataset/Test/Original'
-test_masks_path = 'TissueDataset/Test/Mask'
-# test paths on compute canada machine
-cc_test_frames_path = '../TissueDataset1024/Test/Original'
-cc_test_masks_path = '../TissueDataset1024/Test/Mask'
+    # make predictions
+    assess_models_in_folders(history_dir, test_frames_path, test_masks_path, output_dir)
+    
+    #visualize dataset
+    #visualize_segmentation_dataset(train_frames_path, train_masks_path, 'distortionless')
 
-# output paths
-history_dir = './history_dev/'
-output_dir = './predictions_dev/'
-
-# finetune the mini unet model
-finetuning_random(history_dir, train_frames_path, train_masks_path, val_frames_path, val_masks_path, test_frames_path, test_masks_path, num_models=100)
-#finetuning_loop(history_dir, train_frames_path, train_masks_path, val_frames_path, val_masks_path, test_frames_path, test_masks_path)
-#finetuning_loop(history_dir, cc_train_frames_path, cc_train_masks_path, cc_val_frames_path, cc_val_masks_path, cc_test_frames_path, cc_test_masks_path)
-
-# make predictions
-assess_models_in_folders(history_dir, test_frames_path, test_masks_path, output_dir)
-#assess_models_in_folders(history_dir, cc_test_frames_path, cc_test_masks_path, output_dir)
-
-#visualize dataset
-#visualize_segmentation_dataset(train_frames_path, train_masks_path, 'distortionless')
+main()
